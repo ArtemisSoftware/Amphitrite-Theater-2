30dbbea8089017122996988b793822d916b5dac8
gustavo <gustavo.isaac@gmail.com>
2025-02-06 15:15:46 +0000

    AmphitriteTheaterApplication

---
+++ app/src/main/java/com/artemissoftware/amphitritetheater2/AmphitriteTheaterApplication.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.amphitritetheater2
+
+import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.CachePolicy
+import coil3.util.DebugLogger
+
+class AmphitriteTheaterApplication: Application(),  SingletonImageLoader.Factory {
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader(context).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.1)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+    }
+}
