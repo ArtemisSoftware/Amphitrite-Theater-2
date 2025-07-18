9447306e13ac0988975a8ad420e459b88f5cabf5
gustavo <gustavo.isaac@gmail.com>
2025-07-18 11:34:49 +0100

    ZoomableScreen

---
+++ app/src/main/java/com/artemissoftware/amphitritetheater2/ar/ZoomableScreen.kt
@@ -0,0 +1,35 @@
+package com.artemissoftware.amphitritetheater2.ar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+
+@Composable
+fun ZoomableScreen() {
+
+    Box(modifier = Modifier.fillMaxSize()){
+        ZoomableContent(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            content = {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://i.pinimg.com/736x/19/50/2e/19502e77bc6673f29a9dc6ae2d77e641.jpg")
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Zoomable Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        )
+    }
+}
